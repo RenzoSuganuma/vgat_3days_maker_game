ede34cd520c938a5b3d24634f30c0309719e6071
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-02-25 12:48:46 +0900

    Foundationへ生成レーン情報をAddするように

---
+++ Assets/Script/StageGeneration/ObstacleGenerator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+/// <summary>
+/// ƒŒ[ƒ““à‚ÉáŠQ•¨‚ğ¶¬‚·‚éƒNƒ‰ƒX
+/// </summary>
+public class ObstacleGenerator : MonoBehaviour
+{
+    [SerializeField]
+
+    public void Generate()
+    {
+
+    }
+}
--- Assets/Script/StageGeneration/StageGenerator.cs
+++ Assets/Script/StageGeneration/StageGenerator.cs
@@ -1,20 +1,21 @@
-using NUnit.Framework;
 using System.Collections.Generic;
-using Unity.Burst.CompilerServices;
 using UnityEngine;
 
+/// <summary>
+/// ステージを生成するクラス
+/// </summary>
 public class StageGenerator : MonoBehaviour
 {
     [SerializeField] Transform _player;
     [SerializeField] GameObject _ringPrefab;
+    [SerializeField] IndependenceObstacleGenerator _obstacleGenerator;
 
-    // 高さと間隔の対応
     [SerializeField, Tooltip("基本横幅")] float _baseWidth = 5;
     [SerializeField, Tooltip("階層が上がるごとに増える横幅")] float _widthPerLayer = 1;
     [SerializeField, Tooltip("階層毎の高さの幅")] float _heightPerLayer = 5;
 
-    [SerializeField, Tooltip("生成する階層の数")] int _generateLayers = 30;
-    [SerializeField, Tooltip("Y=0に生成される階層")] int _initialLayer = 10;
+    [SerializeField, Tooltip("生成する階層の数")] int _generateLayers = 6;
+    [SerializeField, Tooltip("Y=0に生成される階層（0-origin）")] int _initialLayer = 2;
     [SerializeField, Tooltip("一度に生成する距離")] float _generateDistance = 100;
     [SerializeField, Tooltip("生成を開始する移動距離")] float _generatePerMoveDistance = 50;
 
@@ -24,11 +25,12 @@ public class StageGenerator : MonoBehaviour
 
     private void Start()
     {
-        for (int layer = 1; layer <= _generateLayers; layer++)
+        for (int layer = 0; layer < _generateLayers; layer++)
         {
             var space = _baseWidth + _widthPerLayer * layer;
             var height = (layer - _initialLayer) * _heightPerLayer;
-            _generator.Add(new StageRowGenerator(_ringPrefab, space, height));
+            _generator.Add(new StageRowGenerator(_ringPrefab, space, height, layer));
+            _obstacleGenerator.StartGenerate(_player, height, layer);
         }
 
         GenerateStage();
@@ -55,36 +57,45 @@ public class StageGenerator : MonoBehaviour
     }
 }
 
+
+/// <summary>
+/// レーン毎にステージを生成するクラス
+/// </summary>
+[System.Serializable] // デバッグ用
 public class StageRowGenerator
 {
     [SerializeField] GameObject _prefab;
-    [SerializeField] Transform _parent;
-    [SerializeField] int _index;
     [SerializeField] float _space;
     [SerializeField] float _height;
+    [SerializeField] int _layer;
 
-    public StageRowGenerator(GameObject prefab, float space, float height)
+    [SerializeField] int _generateIndex; // 生成時に使用するindex
+    [SerializeField] ObstacleGenerator _obstacleGenerator;
+
+    public StageRowGenerator(GameObject prefab, float space, float height, int layer)
     {
         _prefab = prefab;
         _space = space;
         _height = height;
+        _layer = layer;
     }
 
     public void Generate(float maxX, Transform parent = null)
     {
         for (int i = 0; i < 1000; i++)
         {
-            var x = _index * _space;
+            var x = _generateIndex * _space;
             if (x > maxX)
             {
-                return ;
+                return;
             }
 
             var obj = Object.Instantiate(_prefab, new Vector3(x, _height), Quaternion.identity);
+            Foundation.InGameLane[_layer].Add(obj);
 
             if (parent) obj.transform.parent = parent;
 
-            _index++;
+            _generateIndex++;
         }
     }
 }
