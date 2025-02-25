577372cf84c88ab6fca75deccef7c51baa4b4530
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-25 17:17:47 +0900

    [add] ボイスデータに関するクラスを追加

---
+++ Assets/Script/Audio/VoiceData.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ボイスデータ
+/// </summary>
+[System.Serializable]
+public struct VoiceData
+{
+    /// <summary>ボイスデータ（2種類）</summary>
+    [Header("ボイスデータ")] public List<AudioClip> Clips;
+    [Header("音量")] public float Volume;
+}
---
+++ Assets/Script/Audio/VoiceDataSO.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ボイスデータのためのスクリプタブルオブジェクト
+/// </summary>
+[CreateAssetMenu(fileName = "VoiceDataSO", menuName = "Create Scriptable Object/VoiceDataSO")]
+public class VoiceDataSO : ScriptableObject
+{
+    //[SerializeField] private List<VoiceDataSO>
+}
