27e51c3188ff922a8125485912337a77d8e45339
FujitaTokinosuke <gaitengtian6@gmail.com>
2025-02-25 12:49:46 +0900

    UIManager作成

--- Assets/Fujita/Scripts/ScoreManager.cs
+++ Assets/Fujita/Scripts/ScoreManager.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    private int mileage;
 
 }
--- Assets/Fujita/Scripts/UIManager.cs
+++ Assets/Fujita/Scripts/UIManager.cs
@@ -1,8 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    //声の大きさ
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI decibelText;
 
+    //次のセリフText
+    [Header("Next wordsText")]
+    [SerializeField] private TextMeshProUGUI nextWordsText;
+
+    //スコアText
+    [Header("Score")]
+    [SerializeField] private TextMeshProUGUI distanceText;
+    [SerializeField] private TextMeshProUGUI heightText;
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    void Start()
+    {
+        SetUp();
+    }
+
+    void SetUp()
+    {
+        decibelText.text = "0dB";
+        nextWordsText.text = " ";
+        distanceText.text = "0m";
+        heightText.text = "0m";
+        timeText.text = "0";
+    }
 }
