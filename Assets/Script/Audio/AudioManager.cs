38dcd7c74cead0da5f837ece2084ada3afe01722
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-25 17:00:41 +0900

    [add] Audioのクリップ管理用のクラスを作成

---
+++ Assets/Script/Audio/AudioManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+/// <summary>
+/// AudioManager
+/// </summary>
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] private AudioSource _bgmAudioSource;
+    [SerializeField] private AudioSource _seAudioSource;
+
+    [Header("音声データ")]
+    [SerializeField] private ClipDataSO _bgm;
+    [SerializeField] private ClipDataSO _se;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    /// <summary>
+    /// Enumで指定したBGMを再生する
+    /// </summary>
+    public void PlayBGM(SceneNameEnum sceneName)
+    {
+        // SceneNameEnumをint型にキャストして、BGMを再生
+        ClipData data = _bgm.GetClipData((int)sceneName);
+        _bgmAudioSource.clip = data.Clip;
+        _bgmAudioSource.volume = data.Volume;
+    }
+
+    /// <summary>
+    /// Indexで指定したSEを再生する
+    /// </summary>
+    public void PlaySE(int index)
+    {
+        ClipData data = _se.GetClipData(index);
+        _seAudioSource.clip = data.Clip;
+        _seAudioSource.volume = data.Volume;
+    }
+}
---
+++ Assets/Script/Audio/ClipData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+/// <summary>
+/// AudioClipのデータを管理するコンストラクタ
+/// </summary>
+[System.Serializable]
+public struct ClipData
+{
+    [Header("データの名前")] public string ClipName;
+    [Header("クリップ")] public AudioClip Clip;
+    [Header("音量")] public float Volume;
+}
