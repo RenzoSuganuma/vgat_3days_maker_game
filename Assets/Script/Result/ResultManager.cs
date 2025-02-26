0af3c2386a80232eecc8c8c0b312c7bb7a5b9aba
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-27 05:10:24 +0900

    [add] キャラクターのスライドと、カウントアップできるテキストを作成

--- Assets/Script/Result/ResultManager.cs
+++ Assets/Script/Result/ResultManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
@@ -9,22 +10,31 @@ public class ResultManager : MonoBehaviour
 {
     [SerializeField, Tooltip("今回のスコア")] private TextMeshProUGUI _thisScoreText;
     [SerializeField, Tooltip("ランキングのスコア")] private TextMeshProUGUI[] _rankingTexts;
+    [SerializeField] private ResultTween _tween;
     private RankingManager _rankingManager; // ランキングのマネージャークラス
 
     private void Start()
     {
         _rankingManager = new RankingManager();
 
-        _rankingManager.AddScore(1); // 今回のスコアを追加する
-        UpdateResultText(1, _rankingManager.LoadScores()); // UI更新
+        // ランキングのテキストは全て非表示にしておく
+        foreach (TextMeshProUGUI text in _rankingTexts)
+        {
+            text.gameObject.SetActive(false);
+        }
+
+        int score = 10;
+
+        _rankingManager.AddScore(score); // 今回のスコアを追加する
+        UpdateResultText(score, _rankingManager.LoadScores()); // UI更新
     }
 
     /// <summary>
     /// UIの更新を行う
     /// </summary>
-    private void UpdateResultText(int score, List<int> ranking)
+    private async UniTask UpdateResultText(int score, List<int> ranking)
     {
-        _thisScoreText.text = score.ToString() + "m"; // 今回のスコア用
+        await _tween.NumberCounter(score, _thisScoreText); // 今回のスコア用
 
         for (int i = 0; i < _rankingTexts.Length; i++) //ランキング用
         {
@@ -37,6 +47,9 @@ public class ResultManager : MonoBehaviour
             {
                 _rankingTexts[i].text = "";
             }
+
+            _rankingTexts[i].gameObject.SetActive(true); // アクティブにする
+            await UniTask.Delay(500);
         }
     }
 }
--- Assets/Script/ScreenPerformance/ResultTween.cs
+++ Assets/Script/ScreenPerformance/ResultTween.cs
@@ -1,16 +1,49 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Result画面で使用するtweenをまとめたクラス
+/// </summary>
 public class ResultTween : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("キャラクター移動Tweenの設定")]
+    [SerializeField] private Transform _character;
+    [SerializeField, Tooltip("移動量")] private float _movementDistance = 125;
+    [SerializeField, Tooltip("移動にかける時間")] private float _moveDuration = 0.8f;
+
+    [Header("テキストのカウントアップTweenの設定")]
+    [SerializeField] private float _duration = 1.5f; // カウントアップにかける時間
+    private int _startValue = 0;
+
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private void Start()
     {
-        
+        MoveCharacter();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// キャラクターのTween。画面右から左へスライドする
+    /// </summary>
+    private void MoveCharacter()
     {
-        
+        _character.DOLocalMoveX(_character.transform.localPosition.x - _movementDistance, _moveDuration);
+    }
+
+    /// <summary>
+    /// テキストをカウントアップするTween
+    /// </summary>
+    public async UniTask NumberCounter(int endValue, TextMeshProUGUI text)
+    {
+        int currentValue = _startValue;
+
+        DOTween.To(() => currentValue, x => {
+            currentValue = x;
+            text.text = $"{currentValue.ToString()}m";
+        }, endValue, _duration).SetEase(Ease.OutQuad);
+
+        await UniTask.Delay((int)_duration * 1000);
     }
 }
