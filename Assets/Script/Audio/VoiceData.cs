cd3a4e9f7cbb4557d45945bf822c03f243e2b565
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-26 10:37:37 +0900

    [fix] ボイスデータの獲得をintで取得する方式からenumで取得する方式に変更

--- Assets/Script/Audio/VoiceData.cs
+++ Assets/Script/Audio/VoiceData.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public struct VoiceData
 {
     /// <summary>ボイスデータ（2種類）</summary>
+    [Header("ボイスの名前")] public VoiceNameEnum VoiceName;
     [Header("ボイスデータ")] public List<AudioClip> Clips;
     [Header("音量")] public float Volume;
 }
--- Assets/Script/Audio/VoiceDataSO.cs
+++ Assets/Script/Audio/VoiceDataSO.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// <summary>
 /// ボイスデータのためのスクリプタブルオブジェクト
 /// </summary>
-[CreateAssetMenu(fileName = "VoiceDataSO", menuName = "Create Scriptable Object/VoiceDataSO")]
+[CreateAssetMenu(fileName = "VoiceData", menuName = "Create Scriptable Objects/VoiceDataSO")]
 public class VoiceDataSO : ScriptableObject
 {
     [SerializeField] private List<VoiceData> _voiceData = new List<VoiceData>();
@@ -12,17 +12,17 @@ public class VoiceDataSO : ScriptableObject
     /// <summary>
     /// インデックスで指定したボイスデータを返す
     /// </summary>
-    public VoiceData GetVoiceData(int index)
+    public VoiceData GetVoiceData(VoiceNameEnum voiceName)
     {
-        return _voiceData[index];
+        return _voiceData[(int)voiceName];
     }
 
     /// <summary>
     /// インデックスで指定したボイスデータから、ランダムなクリップと設定されたvolumeを返す
     /// </summary>
-    public (AudioClip Clip, float Volume) GetRandomVoiceClip(int index)
+    public (AudioClip Clip, float Volume) GetRandomVoiceClip(VoiceNameEnum voiceName)
     {
-        VoiceData data = _voiceData[index];
+        VoiceData data = _voiceData[(int)voiceName];
         int rand = Random.Range(0, data.Clips.Count);
         return (data.Clips[rand], data.Volume);
     }
