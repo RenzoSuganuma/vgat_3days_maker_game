a4b7b72a2c83d3f21853b2bc29dcb2cefc752fca
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-27 04:44:35 +0900

    [update] Result画面編集

--- Assets/Script/Result/ResultManager.cs
+++ Assets/Script/Result/ResultManager.cs
@@ -1,26 +1,41 @@
-using System.Linq;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Result画面のManagerクラス
+/// </summary>
 public class ResultManager : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI _scoreText;
-    [SerializeField] TextMeshProUGUI[] _rankingText;
+    [SerializeField, Tooltip("今回のスコア")] private TextMeshProUGUI _thisScoreText;
+    [SerializeField, Tooltip("ランキングのスコア")] private TextMeshProUGUI[] _rankingTexts;
+    private RankingManager _rankingManager; // ランキングのマネージャークラス
 
-    public void UpdateResultText(int score, int[] ranking)
+    private void Start()
     {
-        _scoreText.text = score.ToString() + "m";
+        _rankingManager = new RankingManager();
 
-        for (int i = 0; i < _rankingText.Length; i++)
+        _rankingManager.AddScore(1); // 今回のスコアを追加する
+        UpdateResultText(1, _rankingManager.LoadScores()); // UI更新
+    }
+
+    /// <summary>
+    /// UIの更新を行う
+    /// </summary>
+    private void UpdateResultText(int score, List<int> ranking)
+    {
+        _thisScoreText.text = score.ToString() + "m"; // 今回のスコア用
+
+        for (int i = 0; i < _rankingTexts.Length; i++) //ランキング用
         {
-            if (i < ranking.Length)
+            if (i < ranking.Count)
             {
-                _rankingText[i].text = $"{i+1}位 {ranking[i]}m";
-                
+                _rankingTexts[i].text = $"{i+1}位 {ranking[i]}m";
+
             }
             else
             {
-                _rankingText[i].text = "";
+                _rankingTexts[i].text = "";
             }
         }
     }
