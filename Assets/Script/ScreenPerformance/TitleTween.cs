c4b43f4eac2534fa001c044837faaaae78d5aa62
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-27 03:38:59 +0900

    [fix] tweenの設定をinspectorから編集できるように

--- Assets/Script/ScreenPerformance/TitleTween.cs
+++ Assets/Script/ScreenPerformance/TitleTween.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 /// </summary>
 public class TitleTween : MonoBehaviour
 {
+    [Header("ロゴの設定")]
     [SerializeField] private Transform _logo;
+    [SerializeField, Tooltip("移動量(下に下がる)")] private float _amountOfMovement = 10f;
+    [SerializeField] private float _logoAnimDuration = 2f;
+
+    [Header("スタートボタンの設定")]
     [SerializeField] private Transform _startButton;
+    [SerializeField, Tooltip("拡大率")] private float _magnificationRate = 1.05f;
+    [SerializeField] private float _startButtonAnimDuration = 1f;
 
     private void Start()
     {
@@ -28,7 +35,7 @@ public class TitleTween : MonoBehaviour
     /// </summary>
     private void LogoTween()
     {
-        _logo.DOLocalMoveY(_logo.localPosition.y - 10f,2f).SetLoops(-1, LoopType.Yoyo);
+        _logo.DOLocalMoveY(_logo.localPosition.y - _amountOfMovement,_logoAnimDuration).SetLoops(-1, LoopType.Yoyo);
     }
 
     /// <summary>
@@ -36,6 +43,6 @@ public class TitleTween : MonoBehaviour
     /// </summary>
     private void StartButtonTween()
     {
-        _startButton.DOScale(transform.localScale * 1.05f, 1f).SetLoops(-1, LoopType.Yoyo);
+        _startButton.DOScale(transform.localScale * _magnificationRate, _startButtonAnimDuration).SetLoops(-1, LoopType.Yoyo);
     }
 }
