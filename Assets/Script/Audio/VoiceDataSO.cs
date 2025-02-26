bdc53ae50e3d3e2113bdbe318053fa1c5531b90f
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-26 10:30:04 +0900

    [update] ボイスデータの取得用メソッドを追加

--- Assets/Script/Audio/VoiceDataSO.cs
+++ Assets/Script/Audio/VoiceDataSO.cs
@@ -7,5 +7,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "VoiceDataSO", menuName = "Create Scriptable Object/VoiceDataSO")]
 public class VoiceDataSO : ScriptableObject
 {
-    //[SerializeField] private List<VoiceDataSO>
+    [SerializeField] private List<VoiceData> _voiceData = new List<VoiceData>();
+
+    /// <summary>
+    /// インデックスで指定したボイスデータを返す
+    /// </summary>
+    public VoiceData GetVoiceData(int index)
+    {
+        return _voiceData[index];
+    }
+
+    /// <summary>
+    /// インデックスで指定したボイスデータから、ランダムなクリップと設定されたvolumeを返す
+    /// </summary>
+    public (AudioClip Clip, float Volume) GetRandomVoiceClip(int index)
+    {
+        VoiceData data = _voiceData[index];
+        int rand = Random.Range(0, data.Clips.Count);
+        return (data.Clips[rand], data.Volume);
+    }
 }
