0d869f29a9c7076b9a8b7394871f09314d60154c
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-26 17:37:19 +0900

    [add] Audienceがぴょんぴょん跳ねる処理を追加

---
+++ Assets/Script/Camera/AudienceBeatSync.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+using UnityEngine;
+
+/// <summary>
+/// 観客の画像をビートに合わせて移動させる
+/// </summary>
+public class AudienceBeatSync : MonoBehaviour
+{
+    [SerializeField] private Transform _sprite;
+    [SerializeField] private ObjectBeatSync _objectSync;
+
+    private void Start()
+    {
+        _objectSync.OnBeat += OnBeat;
+    }
+
+    private void OnDestroy()
+    {
+        _objectSync.OnBeat -= OnBeat;
+    }
+
+    private void OnBeat()
+    {
+        _sprite.DOMoveY(transform.position.y + 0.2f, 0.1f).SetLoops(2, LoopType.Yoyo);
+    }
+}
