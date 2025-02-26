27169dd9c2949091c8050d0a3e45fff913b0cce2
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2025-02-27 04:17:20 +0900

    [temp]

--- Assets/Script/TitlePanelImpl.cs
+++ Assets/Script/TitlePanelImpl.cs
@@ -1,16 +1,51 @@
+using System;
+using System.Collections.Generic;
+using R3;
 using UnityEngine;
 
 public class TitlePanelImpl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [SerializeField] private CanvasGroup _hideOnScneChange;
+    [SerializeField] private List<GameObject> _ddols = new();
 
+    private void Start()
+    {
+        FindAnyObjectByType<VoiceInputHandler>().IsCorrectVoice.AsObservable().Subscribe(b =>
+        {
+            var hoge = FindAnyObjectByType<VoiceInputHandler>();
+            if (b && hoge.RecognizedText.Value == "スタート")
+                FindAnyObjectByType<SceneLoaderImpl>().StartGame();
+        });
+
+        foreach (var ddol in _ddols)
+        {
+            DontDestroyOnLoad(ddol);
+        }
+
+        Foundation.TaskOnChangedScene += s =>
+        {
+            if (s is Foundation.INGAME_SCENE_NAME)
+            {
+                if (_hideOnScneChange != null)
+                {
+                    _hideOnScneChange.alpha = 0;
+                }
+
+                FindAnyObjectByType<MissionsDisplay>().GetComponent<CanvasGroup>().alpha = 1;
+            }
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FindAnyObjectByType<VoiceInputHandler>().StartSpeechRecognition();
+        }
 
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            FindAnyObjectByType<VoiceInputHandler>().StopSpeechRecognition();
+        }
     }
 }
